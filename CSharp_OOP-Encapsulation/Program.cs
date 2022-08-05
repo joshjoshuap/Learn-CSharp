57dd54b15e1c07e41ce7061a707bf737fa7480c7
Joshua <joshjoshuap1@gmail.com>
2022-08-05 17:31:02 +0800

    encapsulation

---
+++ CSharp_OOP-Encapsulation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharp_OOP_Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account ac = new Account("admin", "123", "Manila");
+            Console.WriteLine($"Username: {ac.UserName} \nPassword: {ac.UserPassword} \nLocation: {ac.userAddress}");
+        }
+    }
+}
