3318ad7f91f339657f72fe59b836d487ec768d5a
Joshua <joshjoshuap1@gmail.com>
2022-08-05 17:31:20 +0800

    constuctors

---
+++ CSharp_OOP-Constructor-Overload/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_OOP_Constructor_Overload
+{
+    class Movie
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public int rating { get; set; }
+        public int price { get; set; }
+
+        public Movie(string title, string description, int rating, int price)
+        {
+            this.title = title;
+            this.description = description;
+            this.rating = rating;
+            this.price = price;
+        }
+
+        // movie without price value or incomplete value but still setting the value
+        public Movie(string title, string description, int rating)
+        {
+            this.title = title;
+            this.description = description;
+            this.rating = rating;
+        }
+    }
+}
---
+++ CSharp_OOP-Constructor-Overload/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp_OOP_Constructor_Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Avengers","Heroes",10, 100);
+            Movie movie2 = new Movie("Minions", "Comedy", 9); // no price
+
+            Console.WriteLine($"Movie: {movie1.title} Genre: {movie1.description} Rate: {movie1.rating} Price: {movie1.price}");
+            // Movie: Avengers Genre: Heroes Rate: 100 Price: 100
+
+            Console.WriteLine($"Movie: {movie2.title} Genre: {movie2.description} Rate: {movie2.rating} Price: {movie2.price}");
+            // Movie: Avengers Genre: Comedy Rate: 9 Price: 0
+        }
+    }
+}
