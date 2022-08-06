a1fe43a92949fbfb4f61d988418d8d0863746fe3
Joshua <joshjoshuap1@gmail.com>
2022-08-06 17:59:13 +0800

    abstraction

---
+++ CSharp_OOP-Abstraction/Instances.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_OOP_Abstraction
+{
+    class Instances
+    {
+    }
+
+    interface School
+    {
+        void displayInfo();
+    }
+
+    class Student : School
+    {
+        public void displayInfo()
+        {
+
+        }
+
+    }
+
+    class Teacher : School
+    {
+        public void displayInfo()
+        {
+
+        }
+
+    }
+}
---
+++ CSharp_OOP-Abstraction/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CSharp_OOP_Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            dog.makeSound();
+            cat.makeSound();
+            /* 
+             arf arf
+            meow meow
+             */
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract void makeSound();
+    }
+
+    class Dog : Animal
+    {
+        public override void makeSound()
+        {
+            Console.WriteLine("arf arf");
+        }
+    }
+
+    class Cat : Animal
+    {
+       public override void makeSound()
+        {
+            Console.WriteLine("meow");
+        }
+    }
+
+}
