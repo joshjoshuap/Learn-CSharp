45647a667d2174c0eb9c0ab191db9354f96e2ec1
Joshua <joshjoshuap1@gmail.com>
2022-08-14 19:46:06 +0800

    todo note

---
+++ TodoNote/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TodoNote.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
