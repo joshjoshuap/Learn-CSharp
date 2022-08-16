157e473286793eab2a557cb8278dc4ec6d68bb4f
Joshua <joshjoshuap1@gmail.com>
2022-08-16 15:10:18 +0800

    delete

--- TodoNote/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TodoNote.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
