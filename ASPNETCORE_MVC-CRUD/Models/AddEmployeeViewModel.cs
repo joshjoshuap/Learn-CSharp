38e9b4ea0aedf5342c0c84867db4d37e2d55f0fc
Joshua <joshjoshuap1@gmail.com>
2022-09-02 16:57:59 +0800

    delete

--- ASPNETCORE_MVC-CRUD/Models/AddEmployeeViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ASPNETCORE_CRUD.Models
-{
-    public class AddEmployeeViewModel
-    {
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Position { get; set; }
-    }
-}
--- ASPNETCORE_MVC-CRUD/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASPNETCORE_CRUD.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
