469e424edcc824bfe215323843756a6ec4ef7f77
Joshua <joshjoshuap1@gmail.com>
2022-09-02 16:58:36 +0800

    rename

---
+++ AspNetCore_MVC-CRUD/Models/AddEmployeeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPNETCORE_CRUD.Models
+{
+    public class AddEmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Position { get; set; }
+    }
+}
---
+++ AspNetCore_MVC-CRUD/Models/DomainModels/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace ASPNETCORE_CRUD.Models.DomainModels
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Position { get; set; }
+    }
+}
